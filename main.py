0297bdd64a98e3c5dd9395c38f1be1c4465cd76b
Denis <117019627+DENIS76T5@users.noreply.github.com>
2023-07-13 20:32:37 +0300

    fatality

--- main.py
+++ main.py
@@ -15,21 +15,24 @@ options = webdriver.FirefoxOptions()
 options.add_argument("-profile")
 options.add_argument("/Users/mac/library/Application Support/Firefox/profiles/du9nwate.default-release-1")
 options.set_preference("dom.webdriver.enabled", False)
+# options.add_argument("-headless")
 options.set_preference("general.useragent.override", user_agent)
 driver = webdriver.Firefox(options=options)
+
 url = "https://www.instagram.com"
-url2 ="https://opensea.io/"
+url2 = "https://opensea.io/"
 url3 = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
 url4 = "https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html"
+
 try:
     driver.get(url4)
     driver.fullscreen_window()
-    time.sleep(2)
-
-    time.sleep(10)
-
-
-# pickle.dump(driver.get_cookies(), open("cookie", "wb"))
+    driver.implicitly_wait(2)
+    click('/html/body/div[1]/div/div[1]/div/nav/ul/div/div/div/div/div/button/span')
+    driver.implicitly_wait(4)
+    click("/html/body/div[5]/div/div/div/section/div/div[2]/div/ul/li[1]/button/div[2]")
+    # driver.switch_to.window(driver.window_handles[1])
+    time.sleep(50)
 
 except Exception as e:
     print(e)    
@@ -37,3 +40,9 @@ except Exception as e:
 finally:
     driver.close()
     driver.quit()    
+
+
+
+# for cookie in pickle.load(open ("cookie", "rb")):
+    # driver.add_cookie(cookie)
+# pickle.dump(driver.get_cookies(), open("cookie", "wb"))
