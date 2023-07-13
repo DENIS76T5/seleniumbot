309cffd5366e195e194b0c7a7a627f48f3fadcb2
Denis <117019627+DENIS76T5@users.noreply.github.com>
2023-07-13 12:49:19 +0300

    kaka

--- main.py
+++ main.py
@@ -2,44 +2,37 @@
 from selenium import webdriver
 from selenium.webdriver.common import by
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.firefox.options import Options
 import time, pickle
 from fake_useragent import UserAgent
 user_agent = UserAgent().random
 
-options = Options()
+def click(x_path):
+    button = driver.find_element(by.By.XPATH, x_path)
+    button.click()
+
+options = webdriver.FirefoxOptions()
 options.add_argument("-profile")
-options.add_argument("/Users/mac/library/Application Support/firefox/profiles/z8jk1441.default-release-1")
+options.add_argument("/Users/mac/library/Application Support/Firefox/profiles/du9nwate.default-release-1")
+options.set_preference("dom.webdriver.enabled", False)
 options.set_preference("general.useragent.override", user_agent)
 driver = webdriver.Firefox(options=options)
 url = "https://www.instagram.com"
 url2 ="https://opensea.io/"
 url3 = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
+url4 = "https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html"
 try:
-    driver.get(url2)
+    driver.get(url4)
     driver.fullscreen_window()
     time.sleep(2)
 
-
-    driver.find_element(by.By.XPATH, "/html/body/div[1]/div/div[1]/div/nav/ul/div/div/div/div/div/button").click()
-    time.sleep(1)
-    driver.find_element(by.By.XPATH, "/html/body/div[5]/div/div/div/section/div/div[2]/div/ul/li[1]/button/div[2]").click()
-    time.sleep(3)
-    pidor = driver.find_element(by.By.XPATH, "//*[@id=\"password\"]")
-    # pidor.send_keys("thWHL4HuM_vx-PF")
-    # pickle.dump(driver.get_cookies(), open("cookie", "wb"))
     time.sleep(10)
 
 
+# pickle.dump(driver.get_cookies(), open("cookie", "wb"))
 
-
-
-
-
-
-#     time.sleep(202)
-# except Exception as e:
-#     print(e)    
+except Exception as e:
+    print(e)    
 
 finally:
     driver.close()
